5ef4e59e2fa529625131d53c8ee8743ded2a7ade
lijia-wei <1436464371@qq.com>
2021-06-03 16:50:06 +0800

    配置

--- myproject/src/main.js
+++ myproject/src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+import './plugin/element-ui'
+import ElementUI from 'element-ui';
+import 'font-awesome/css/font-awesome.min.css'
+
+//引入让vue能使用axios不能携带cookie
+axios.defaults.withCredentials = true;    
+
+Vue.use(Vuex)  
+
+// aiox需要的包
+import axios from 'axios'
+//aciox基础url
+axios.defaults.baseURL = 'http://47.108.190.196/QAPlatform';
+//将axios写成vue的原型属性
+Vue.prototype.$axios = axios;
+//为post请求设置默认请求头
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
+
+Vue.config.productionTip = false;
+
+//JSON格式时间转换为标准格式时间
+import moment from 'moment/moment';
+Vue.prototype.$moment = moment;
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router: router,
+  store,
+  render: h => h(App), 
+})
+  
--- myproject/src/router/index.js
+++ myproject/src/router/index.js
@@ -2,23 +2,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import index from '../views/index'
+// import index from '../views/index'
 import login from '../views/Login'
-import register from '../views/Register'
-import about from '../views/Aboutus'
-import myquestions from '../views/personalCenter/Personalquestions'
-import setting from '../views/personalCenter/Personalset'
+// import register from '../views/Register'
+// import about from '../views/Aboutus'
+// import myquestions from '../views/personalCenter/Personalquestions'
+// import setting from '../views/personalCenter/Personalset'
 //2.通过Vue.use()安装插件
 Vue.use(VueRouter)
 
 //3.创建VueRouter对象
 const routes = [
-  { path: '/', component: index, meta:{title: '首页'}}, 
-  { path: '/register', component: register, meta:{title: '注册'} },
-  { path: '/login', component: login, meta:{title: '登录'} },
-  { path: '/myquestions', component: myquestions, meta:{title: '我的提问'} },
-  { path: '/setting', component: setting, meta:{title: '个人中心'} },
-  { path: '/about', component: about, meta:{title: '关于我们'} },
+  { path: '/', component: login, meta:{title: '登录'} },
+  // { path: '/', component: index, meta:{title: '首页'}}, 
+  // { path: '/register', component: register, meta:{title: '注册'} },
+  
+  // { path: '/myquestions', component: myquestions, meta:{title: '我的提问'} },
+  // { path: '/setting', component: setting, meta:{title: '个人中心'} },
+  // { path: '/about', component: about, meta:{title: '关于我们'} },
 ]
 
 const router = new VueRouter({
